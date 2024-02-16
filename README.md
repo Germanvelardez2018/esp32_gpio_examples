95baabdde2a8073f7f0d61b8f258d4ff4b9237aa
German Velardez <gvelardez@inti.gob.ar>
2024-02-16 17:11:48 -0300

    [Led timer] Led blinking using a esp timer

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 At the moment, the project is a simple LED blink on GPIO 2 every second. In the future, we will advance in complexity by utilizing the GPIO driver as well as other drivers available in the Espressif SDK (ESP-IDF).
+
+
+TAG: led blink: Simple led blinking
+
+TAG led timer: Led blinking use ESPTimer
--- main/main.c
+++ main/main.c
@@ -3,21 +3,47 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "driver/gpio.h"
+#include "driver/gptimer.h"
+#include "esp_timer.h"
 
 #define LED_GPIO_ESP32      2
 
 
+uint64_t time = 0 ;
+bool toggle = false;
+
+void timer_cb(void*){
+        toggle = (toggle  == true)?     false:    true;
+        gpio_set_level(LED_GPIO_ESP32, toggle);
+}
+
+static esp_timer_create_args_t cfg = {.callback = timer_cb,
+                                      .arg  = NULL,
+                                      .name = "TIMER A",
+                                      .dispatch_method = ESP_TIMER_TASK,
+                                      .skip_unhandled_events = true};
+static esp_timer_handle_t timerA ;
+
 
 void app_main(void)
 {
+    // Create a timer
+    esp_err_t ret;
     gpio_set_direction(LED_GPIO_ESP32,GPIO_MODE_DEF_OUTPUT);
     printf("Init esp32\r\n");
-    bool toggle = false;
-    for (int i = 200; i >= 0; i--) {
-            printf("Restarting in %d seconds...\n", i);
-            vTaskDelay(1000 / portTICK_PERIOD_MS);
-            toggle = (toggle  == true)?     false:    true;
-            gpio_set_level(LED_GPIO_ESP32, toggle);
+
+    ret = esp_timer_create(&cfg,&timerA);
+    if(ret != ESP_OK)printf("Error al iniciar el timer \r\n");
+    ret = esp_timer_start_periodic(timerA,1000000);
+    if(ret != ESP_OK)printf("Error al iniciar el timer \r\n");
+    
+    uint64_t base = esp_timer_get_time();
+    for (int i = 0; i <= 200; i++) {
+         
+          vTaskDelay(1000 / portTICK_PERIOD_MS);
+         
+          time = esp_timer_get_time();
+          printf("  Counter = %lld  seconds...\r\n", ((time-base)/1000000));
 
         }
 
